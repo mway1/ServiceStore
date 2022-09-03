2de2e0805b6ffcda8e4f659b0ee79c0d0396d04f
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-03 13:42:55 +0300

    add Lead_Delete stored procedure

---
+++ ServiceStore/ServiceStore.DB/dbo/Stored Procedures/Lead_Delete.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[Lead_Delete]
+	@Id int,
+    @LeadId varchar(50),
+    @SubStartDate date,
+    @SubEndDate date,
+    @ServiceId int,
+    @isDeleted bit
+AS
+BEGIN
+
+UPDATE dbo.Lead
+SET	LeadId=@LeadId,
+    SubStartDate=@SubStartDate,
+    SubEndDate=@SubEndDate,
+    ServiceId=@ServiceId,
+	IsDeleted=1
+WHERE Id = @Id
+
+END
