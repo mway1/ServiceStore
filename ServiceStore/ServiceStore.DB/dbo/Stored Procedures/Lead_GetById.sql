f730b7c7d641dc283043b058998b05db83cefcb7
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-03 13:38:54 +0300

    add Lead_GetById stored procedure

---
+++ ServiceStore/ServiceStore.DB/dbo/Stored Procedures/Lead_GetById.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Lead_GetById]
+	@Id int,
+    @LeadId varchar(50),
+    @SubStartDate date,
+    @SubEndDate date,
+    @ServiceId int,
+    @isDeleted bit
+AS
+BEGIN
+	SELECT Id,LeadId,SubStartDate,SubEndDate,ServiceId,isDeleted
+	FROM dbo.Lead
+	WHERE Id=@Id
+END
