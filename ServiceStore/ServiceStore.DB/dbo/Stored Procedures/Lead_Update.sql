83abc65e3bef7652e323bfbb6955d81964ddad2a
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-03 13:40:59 +0300

    add Lead_Update stored procedure

---
+++ ServiceStore/ServiceStore.DB/dbo/Stored Procedures/Lead_Update.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[Lead_Update]
+	@Id int,
+    @LeadId varchar(50),
+    @SubStartDate date,
+    @SubEndDate date,
+    @ServiceId int,
+    @isDeleted bit
+AS
+BEGIN
+
+UPDATE dbo.Lead
+SET LeadId=@LeadId,
+    SubStartDate=@SubStartDate,
+    SubEndDate=@SubEndDate,
+    ServiceId=@ServiceId,
+	isDeleted=@isDeleted
+WHERE Id = @Id
+
+END
