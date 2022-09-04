398ff9de22f1943020473b9648dc04bc4b4755f7
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-05 01:45:55 +0300

    add Service_GetById stored procedure

---
+++ ServiceStore/ServiceStore.DB/dbo/Stored Procedures/Service_GetById.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Service_GetById]
+  @Id int,
+  @Name varchar(100),
+  @Description text,
+  @TypeId int,
+  @Price decimal(18),
+  @isDeleted bit
+AS
+BEGIN
+	SELECT Id,Name,Description,TypeId,Price,isDeleted
+	FROM dbo.Service
+	WHERE Id=@Id
+END
