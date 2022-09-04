c2719dda6ca29953bfcdee1b9d44b3dd0a23efd2
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-05 01:38:44 +0300

    add Service_Add stored procedure

---
+++ ServiceStore/ServiceStore.DB/dbo/Stored Procedures/Service_Add.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [dbo].[Service_Add]
+  @Id int,
+  @Name varchar(100),
+  @Description text,
+  @TypeId int,
+  @Price decimal(18),
+  @isDeleted bit
+AS
+BEGIN
+INSERT INTO dbo.Service(
+        Id,
+        Name,
+        Description,
+        TypeId,
+        Price,
+		isDeleted)
+VALUES(
+    @Id,
+    @Name,
+    @Description,
+    @TypeId,
+    @Price,
+	@isDeleted)
+SET @Id=SCOPE_IDENTITY()
+
+SELECT
+    Id=@Id,
+    Name=@Name,
+    Description=@Description,
+    TypeId=@TypeId,
+    Price=@Price,
+	isDeleted=@isDeleted
+FROM dbo.Service
+END
