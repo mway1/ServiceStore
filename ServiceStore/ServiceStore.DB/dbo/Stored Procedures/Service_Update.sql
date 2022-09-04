7b7aea60c8dfbe507a1d2b054708ee1f4fd7ffcd
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-05 01:48:30 +0300

    add Service_Update stored procedure

---
+++ ServiceStore/ServiceStore.DB/dbo/Stored Procedures/Service_Update.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[Service_Update]
+  @Id int,
+  @Name varchar(100),
+  @Description text,
+  @TypeId int,
+  @Price decimal(18),
+  @isDeleted bit
+AS
+BEGIN
+
+UPDATE dbo.Service
+SET Name=@Name,
+    Description=@Description,
+    TypeId=@TypeId,
+    Price=@Price,
+	isDeleted=@isDeleted
+WHERE Id = @Id
+
+END
