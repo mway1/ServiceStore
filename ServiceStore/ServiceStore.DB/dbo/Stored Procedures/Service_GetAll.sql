0a4d5c95df2ee02233d74089d7080480b8315835
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-05 01:42:28 +0300

    add Service_GetAll stored procedure

---
+++ ServiceStore/ServiceStore.DB/dbo/Stored Procedures/Service_GetAll.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Service_GetAll]
+AS
+BEGIN
+
+	SELECT S.Id,S.Name,S.Description,T.Name,S.Price,S.isDeleted
+	FROM dbo.Service as S
+	LEFT JOIN dbo.TypeOfService as T on (T.Id=S.TypeId)
+END
