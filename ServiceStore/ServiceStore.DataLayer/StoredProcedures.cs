923239466581195aa3df2c38534ab4f7cbc74ff4
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-08 13:21:38 +0300

    add const for Lead stored procedures

---
+++ ServiceStore/ServiceStore.DataLayer/StoredProcedures.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceStore.DataLayer
+{
+    public static class StoredProcedures
+    {
+        public const string Lead_Add = "Lead_Add";
+        public const string Lead_Delete = "Lead_Delete";
+        public const string Lead_GetAll = "Lead_GetAll";
+        public const string Lead_GetById = "Lead_GetById";
+        public const string Lead_Update = "Lead_Update";
+    }
+}
