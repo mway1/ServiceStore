9471e9bf5bd19729f7c0b510e9ef1fc12b2d43f3
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-08 20:45:52 +0300

    Fill 'Add' method for LeadRepository and change namespaces

--- ServiceStore/ServiceStore.DataLayer/Models/TypeOfServiceDTO.cs
+++ ServiceStore/ServiceStore.DataLayer/Models/TypeOfServiceDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ServiceStore.DataLayer.Models
+namespace ServiceStore.DataLayer
 {
     public class TypeOfServiceDTO
     {
--- ServiceStore/ServiceStore.DataLayer/Repositories/LeadRepository.cs
+++ ServiceStore/ServiceStore.DataLayer/Repositories/LeadRepository.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using System.Data;
 
-namespace ServiceStore.DataLayer.Repositories
+namespace ServiceStore.DataLayer
 {
-    internal class LeadRepository
+    public class LeadRepository : BaseRepository, ILeadRepository
     {
+        public LeadRepository(IDbConnection connection) : base(connection)
+        {
+
+        }
+
+        public async Task<int> Add(LeadDTO leadDto)
+        {
+            var id = await _connectionString.QuerySingleAsync<int>(
+            StoredProcedures.Lead_Add,
+            param: new
+            {
+                leadDto.Id,
+                leadDto.LeadId,
+                leadDto.SubStartDate,
+                leadDto.SubEndDate,
+                leadDto.ServiceId
+            },
+            commandType: System.Data.CommandType.StoredProcedure);
+
+            return id;
+        }
+
+        public Task DeleteOrRestore(int id, bool isDeleting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LeadDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeadDTO> GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeadDTO> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(LeadDTO leadDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
