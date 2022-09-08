a39da22ba9373bf6d1de57d89d62bec9a1ea055d
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-08 20:06:49 +0300

    Add BaseRepository

---
+++ ServiceStore/ServiceStore.DataLayer/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ServiceStore.DataLayer
+{
+    public class BaseRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public BaseRepository(IDbConnection dbConnection)
+        {
+            _connection = dbConnection;
+        }
+
+        protected IDbConnection _connectionString => new SqlConnection(_connection.ConnectionString);
+    }
+}
