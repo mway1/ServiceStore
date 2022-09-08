e74c13905f275c35ac631e75c5b2665245d1002f
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-08 13:23:29 +0300

    add const for Service stored procedures

--- ServiceStore/ServiceStore.DataLayer/StoredProcedures.cs
+++ ServiceStore/ServiceStore.DataLayer/StoredProcedures.cs
@@ -13,5 +13,11 @@ namespace ServiceStore.DataLayer
         public const string Lead_GetAll = "Lead_GetAll";
         public const string Lead_GetById = "Lead_GetById";
         public const string Lead_Update = "Lead_Update";
+
+        public const string Service_Add = "Service_Add";
+        public const string Service_Delete = "Service_Delete";
+        public const string Service_GetAll = "Service_GetAll";
+        public const string Service_GetById = "Service_GetById";
+        public const string Service_Update = "Service_Update";
     }
 }
