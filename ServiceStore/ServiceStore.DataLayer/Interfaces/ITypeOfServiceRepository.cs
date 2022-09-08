1bd9aa4d319c379551b41d5f90bac93bd966fbc9
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-08 20:51:04 +0300

    Some changes

--- ServiceStore/ServiceStore.DataLayer/Interfaces/ITypeOfServiceRepository.cs
+++ ServiceStore/ServiceStore.DataLayer/Interfaces/ITypeOfServiceRepository.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServiceStore.DataLayer.Interfaces
+﻿namespace ServiceStore.DataLayer
 {
-    internal class ITypeOfServiceRepository
+    public interface ITypeOfServiceRepository
     {
     }
 }
