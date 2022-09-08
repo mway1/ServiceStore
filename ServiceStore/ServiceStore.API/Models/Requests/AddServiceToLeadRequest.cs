f2d37502807cd46ce37be42aa02dac559dbe5da2
Maxim Kuzmenko <mway2914@yandex.ru>
2022-09-08 11:15:45 +0300

    add AddServiceToLeadRequest/Response

---
+++ ServiceStore/ServiceStore.API/Models/Requests/AddServiceToLeadRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceStore.API.Models.Request
+{
+    public class AddServiceToLeadRequest
+    {
+        public int LeadId { get; set; }
+        public int ServiceId { get; set; }
+        public DateOnly SubStartDate { get; set; }
+        public DateOnly SubEndDate { get; set; }
+    }
+}
---
+++ ServiceStore/ServiceStore.API/Models/Responses/AddServiceToLeadResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceStore.API.Models.Responses
+{
+    public class AddServiceToLeadResponse
+    {
+        public int LeadId { get; set; }
+        public int ServiceId { get; set; }
+        public DateOnly SubEndDate { get; set; }
+    }
+}
