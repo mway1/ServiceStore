cafcbbb1ace92322f6fcb1bba4da73e3674a2161
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-09 12:22:42 +0300

    Fill interface 'ITypeOfServiceRepository

--- ServiceStore/ServiceStore.DataLayer/Interfaces/ITypeOfServiceRepository.cs
+++ ServiceStore/ServiceStore.DataLayer/Interfaces/ITypeOfServiceRepository.cs
@@ -2,5 +2,10 @@
 {
     public interface ITypeOfServiceRepository
     {
+        Task<int> Add(TypeOfServiceDTO typeOfServiceDto);
+        Task<List<TypeOfServiceDTO>> GetAll();
+        Task<TypeOfServiceDTO> GetById(int id);
+        Task Update(TypeOfServiceDTO typeOfServiceDto);
+        Task Delete(int id, bool isDeleting);
     }
 }
