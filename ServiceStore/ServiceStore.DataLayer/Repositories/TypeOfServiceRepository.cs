8b31b3076071fbadf9b33ca5be2add6b8e0165b4
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-09 12:33:21 +0300

    Fill all methods for TypeOfServiceRepository

--- ServiceStore/ServiceStore.DataLayer/Repositories/TypeOfServiceRepository.cs
+++ ServiceStore/ServiceStore.DataLayer/Repositories/TypeOfServiceRepository.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using System.Data;
 
-namespace ServiceStore.DataLayer.Repositories
+namespace ServiceStore.DataLayer
 {
-    internal class TypeOfServiceRepository
+    public class TypeOfServiceRepository : BaseRepository, ITypeOfServiceRepository
     {
+        public TypeOfServiceRepository(IDbConnection connection) : base(connection)
+        {
+
+        }
+
+        public async Task<int> Add(TypeOfServiceDTO typeOfServiceDto)
+        {
+            var id = await _connectionString.QuerySingleAsync<int>(
+           StoredProcedures.TypeOfService_Add,
+           param: new
+           {
+               typeOfServiceDto.Id,
+               typeOfServiceDto.Name,            
+               typeOfServiceDto.isDeleted
+           },
+           commandType: System.Data.CommandType.StoredProcedure);
+
+            return id;
+        }
+
+        public async Task Delete(int id, bool isDeleting)
+        {
+            await _connectionString.QueryFirstOrDefaultAsync<TypeOfServiceDTO>(
+                 StoredProcedures.TypeOfService_Delete,
+                 param: new { id },
+                 commandType: System.Data.CommandType.StoredProcedure);
+        }
+
+        public async Task<List<TypeOfServiceDTO>> GetAll()
+        {
+            var typesOfServices = _connectionString.Query<TypeOfServiceDTO>(
+            StoredProcedures.TypeOfService_GetAll,
+            commandType: System.Data.CommandType.StoredProcedure)
+            .ToList();
+
+            return typesOfServices;
+        }
+
+        public async Task<TypeOfServiceDTO> GetById(int id)
+        {
+            var typeOfService = await _connectionString.QueryFirstOrDefaultAsync<TypeOfServiceDTO>(
+           StoredProcedures.TypeOfService_GetById,
+           param: new { id },
+           commandType: System.Data.CommandType.StoredProcedure);
+
+            return typeOfService;
+        }
+
+        public async Task Update(TypeOfServiceDTO typeOfServiceDto)
+        {
+            await _connectionString.QueryFirstOrDefaultAsync<TypeOfServiceDTO>(
+            StoredProcedures.TypeOfService_Update,
+            param: new
+            {
+                typeOfServiceDto.Id,
+                typeOfServiceDto.Name,
+                typeOfServiceDto.isDeleted
+            },
+            commandType: System.Data.CommandType.StoredProcedure);
+        }
     }
 }
