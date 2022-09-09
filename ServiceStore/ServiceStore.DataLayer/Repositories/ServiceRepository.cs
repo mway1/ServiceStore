655656f57fa5b51d2ca12a7ea65a058e73277119
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-09 12:10:01 +0300

    Add methods for LeadRepository

--- ServiceStore/ServiceStore.DataLayer/Repositories/ServiceRepository.cs
+++ ServiceStore/ServiceStore.DataLayer/Repositories/ServiceRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace ServiceStore.DataLayer
+﻿using Dapper;
+using System.Data;
+
+namespace ServiceStore.DataLayer
 {
-    internal class ServiceRepository
+    public class ServiceRepository : BaseRepository, IServiceRepository
     {
+        public ServiceRepository(IDbConnection connection) : base(connection)
+        {
+
+        }
+
+        public Task<int> Add(ServiceDTO leadDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id, bool isDeleting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ServiceDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceDTO> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(ServiceDTO leadDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
